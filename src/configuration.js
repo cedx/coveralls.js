b6dff8168c9c33fbdc1b6b9b1ec9bd92ded78d10
Cédric Belin <cedric@belin.io>
2017-01-23 14:55:12 +0100

    Added the `Configuration` class

---
+++ src/configuration.js
@@ -0,0 +1,37 @@
+/**
+ * Provides access to the coverage settings.
+ */
+export class Configuration {
+
+  /**
+   * Initializes a new instance of the class.
+   * @param {object} [params] The configuration parameters.
+   */
+  constructor(params = {}) {
+
+    /**
+     * The configuration parameters.
+     * @type {object}
+     */
+    this._params = params;
+  }
+
+  /**
+   * Gets the value of the configuration parameter with the specified name.
+   * @param {string} name The name of the configuration parameter.
+   * @param {*} defaultValue The default parameter value if it does not exist.
+   * @return {*} The value of the configuration parameter, or the default value if the parameter is not found.
+   */
+  get(name, defaultValue = undefined) {
+    return name in this._params ? this._params[name] : defaultValue;
+  }
+
+  /**
+   * Sets the value of the configuration parameter with the specified name.
+   * @param {string} name The name of the configuration parameter.
+   * @param {*} value The parameter value.
+   */
+  set(name, value) {
+    this._params[name] = value;
+  }
+}
