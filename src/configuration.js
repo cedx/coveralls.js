9f2ffe654e61a32ce7d6e21fd311fe3f1e0e0c3b
Cédric Belin <cedric@belin.io>
2017-01-25 23:30:08 +0100

    Code factorization

--- src/configuration.js
+++ src/configuration.js
@@ -32,59 +32,58 @@ export class Configuration {
 
   /**
    * Creates a new configuration from the variables of the specified environment.
-   * @param {object} [environment] A map providing environment variables. Defaults to `process.env`.
+   * @param {object} [env] A map providing environment variables. Defaults to `process.env`.
    * @return {Configuration} The newly created configuration.
    */
-  static fromEnvironment(environment = null) {
+  static fromEnvironment(env = process.env) {
     let config = new Configuration();
-    if (!environment || typeof environment != 'object') environment = process.env;
 
     // Standard.
-    let serviceName = typeof environment.CI_NAME == 'string' ? environment.CI_NAME : '';
+    let serviceName = typeof env.CI_NAME == 'string' ? env.CI_NAME : '';
     if (serviceName.length) config.set('service_name', serviceName);
 
-    if ('CI_BRANCH' in environment) config.set('service_branch', environment.CI_BRANCH);
-    if ('CI_BUILD_NUMBER' in environment) config.set('service_number', environment.CI_BUILD_NUMBER);
-    if ('CI_BUILD_URL' in environment) config.set('service_build_url', environment.CI_BUILD_URL);
-    if ('CI_COMMIT' in environment) config.set('commit_sha', environment.CI_COMMIT);
-    if ('CI_JOB_ID' in environment) config.set('service_job_id', environment.CI_JOB_ID);
+    if ('CI_BRANCH' in env) config.set('service_branch', env.CI_BRANCH);
+    if ('CI_BUILD_NUMBER' in env) config.set('service_number', env.CI_BUILD_NUMBER);
+    if ('CI_BUILD_URL' in env) config.set('service_build_url', env.CI_BUILD_URL);
+    if ('CI_COMMIT' in env) config.set('commit_sha', env.CI_COMMIT);
+    if ('CI_JOB_ID' in env) config.set('service_job_id', env.CI_JOB_ID);
 
-    if ('CI_PULL_REQUEST' in environment) {
-      let matches = /(\d+)$/.exec(environment.CI_PULL_REQUEST);
+    if ('CI_PULL_REQUEST' in env) {
+      let matches = /(\d+)$/.exec(env.CI_PULL_REQUEST);
       if (matches && matches.length >= 2) config.set('service_pull_request', matches[1]);
     }
 
     // Coveralls.
-    if ('COVERALLS_REPO_TOKEN' in environment || 'COVERALLS_TOKEN' in environment)
-      config.set('repo_token', environment.COVERALLS_REPO_TOKEN ? environment.COVERALLS_REPO_TOKEN : environment.COVERALLS_TOKEN);
+    if ('COVERALLS_REPO_TOKEN' in env || 'COVERALLS_TOKEN' in env)
+      config.set('repo_token', env.COVERALLS_REPO_TOKEN ? env.COVERALLS_REPO_TOKEN : env.COVERALLS_TOKEN);
 
-    if ('COVERALLS_COMMIT_SHA' in environment) config.set('commit_sha', environment.COVERALLS_COMMIT_SHA);
-    if ('COVERALLS_PARALLEL' in environment) config.set('parallel', environment.COVERALLS_PARALLEL);
-    if ('COVERALLS_RUN_AT' in environment) config.set('run_at', environment.COVERALLS_RUN_AT);
-    if ('COVERALLS_SERVICE_BRANCH' in environment) config.set('service_branch', environment.COVERALLS_SERVICE_BRANCH);
-    if ('COVERALLS_SERVICE_JOB_ID' in environment) config.set('service_job_id', environment.COVERALLS_SERVICE_JOB_ID);
-    if ('COVERALLS_SERVICE_NAME' in environment) config.set('service_name', environment.COVERALLS_SERVICE_NAME);
+    if ('COVERALLS_COMMIT_SHA' in env) config.set('commit_sha', env.COVERALLS_COMMIT_SHA);
+    if ('COVERALLS_PARALLEL' in env) config.set('parallel', env.COVERALLS_PARALLEL);
+    if ('COVERALLS_RUN_AT' in env) config.set('run_at', env.COVERALLS_RUN_AT);
+    if ('COVERALLS_SERVICE_BRANCH' in env) config.set('service_branch', env.COVERALLS_SERVICE_BRANCH);
+    if ('COVERALLS_SERVICE_JOB_ID' in env) config.set('service_job_id', env.COVERALLS_SERVICE_JOB_ID);
+    if ('COVERALLS_SERVICE_NAME' in env) config.set('service_name', env.COVERALLS_SERVICE_NAME);
 
     // Git.
-    if ('GIT_AUTHOR_EMAIL' in environment) config.set('git_author_email', environment.GIT_AUTHOR_EMAIL);
-    if ('GIT_AUTHOR_NAME' in environment) config.set('git_author_name', environment.GIT_AUTHOR_NAME);
-    if ('GIT_BRANCH' in environment) config.set('service_branch', environment.GIT_BRANCH);
-    if ('GIT_COMMITTER_EMAIL' in environment) config.set('git_committer_email', environment.GIT_COMMITTER_EMAIL);
-    if ('GIT_COMMITTER_NAME' in environment) config.set('git_committer_name', environment.GIT_COMMITTER_NAME);
-    if ('GIT_ID' in environment) config.set('commit_sha', environment.GIT_ID);
-    if ('GIT_MESSAGE' in environment) config.set('git_message', environment.GIT_MESSAGE);
+    if ('GIT_AUTHOR_EMAIL' in env) config.set('git_author_email', env.GIT_AUTHOR_EMAIL);
+    if ('GIT_AUTHOR_NAME' in env) config.set('git_author_name', env.GIT_AUTHOR_NAME);
+    if ('GIT_BRANCH' in env) config.set('service_branch', env.GIT_BRANCH);
+    if ('GIT_COMMITTER_EMAIL' in env) config.set('git_committer_email', env.GIT_COMMITTER_EMAIL);
+    if ('GIT_COMMITTER_NAME' in env) config.set('git_committer_name', env.GIT_COMMITTER_NAME);
+    if ('GIT_ID' in env) config.set('commit_sha', env.GIT_ID);
+    if ('GIT_MESSAGE' in env) config.set('git_message', env.GIT_MESSAGE);
 
     // CI services.
-    if ('TRAVIS' in environment) config.merge(travis_ci.getConfiguration());
-    else if ('APPVEYOR' in environment) config.merge(appveyor.getConfiguration());
-    else if ('CIRCLECI' in environment) config.merge(circleci.getConfiguration());
+    if ('TRAVIS' in env) config.merge(travis_ci.getConfiguration());
+    else if ('APPVEYOR' in env) config.merge(appveyor.getConfiguration());
+    else if ('CIRCLECI' in env) config.merge(circleci.getConfiguration());
     else if (serviceName == 'codeship') config.merge(codeship.getConfiguration());
-    else if ('GITLAB_CI' in environment) config.merge(gitlab_ci.getConfiguration());
-    else if ('JENKINS_URL' in environment) config.merge(jenkins.getConfiguration());
-    else if ('SEMAPHORE' in environment) config.merge(semaphore.getConfiguration());
-    else if ('SURF_SHA1' in environment) config.merge(surf.getConfiguration());
-    else if ('TDDIUM' in environment) config.merge(solano_ci.getConfiguration());
-    else if ('WERCKER' in environment) config.merge(wercker.getConfiguration());
+    else if ('GITLAB_CI' in env) config.merge(gitlab_ci.getConfiguration());
+    else if ('JENKINS_URL' in env) config.merge(jenkins.getConfiguration());
+    else if ('SEMAPHORE' in env) config.merge(semaphore.getConfiguration());
+    else if ('SURF_SHA1' in env) config.merge(surf.getConfiguration());
+    else if ('TDDIUM' in env) config.merge(solano_ci.getConfiguration());
+    else if ('WERCKER' in env) config.merge(wercker.getConfiguration());
 
     return config;
   }
@@ -95,8 +94,11 @@ export class Configuration {
    * @return {Configuration} The instance corresponding to the specified YAML document, or `null` if a parsing error occurred.
    */
   static fromYAML(document) {
+    if (typeof document != 'string' || !document.length) return null;
+
     try {
-      return typeof document == 'string' && document.length ? new Configuration(loadYAML(document)) : null;
+      let map = loadYAML(document);
+      return map && typeof map == 'object' ? new Configuration(map) : null;
     }
 
     catch (err) {
@@ -113,21 +115,22 @@ export class Configuration {
   }
 
   /**
-   * Gets the value of the configuration parameter with the specified name.
-   * @param {string} name The name of the configuration parameter.
+   * Gets the value associated to the specified key.
+   * @param {string} key The key to seek for.
    * @param {*} defaultValue The default parameter value if it does not exist.
    * @return {*} The value of the configuration parameter, or the default value if the parameter is not found.
    */
-  get(name, defaultValue = null) {
-    return typeof this._params[name] != 'undefined' ? this._params[name] : defaultValue;
+  get(key, defaultValue = null) {
+    return typeof this._params[key] != 'undefined' ? this._params[key] : defaultValue;
   }
 
   /**
    * Loads the default configuration.
    * The default values are read from the `.coveralls.yml` file and the environment variables.
+   * @param {string} [coverallsFile] The path to an optional `.coveralls.yml` file. Defaults to the file found in the current directory.
    * @return {Promise<Configuration>} The default configuration.
    */
-  static loadDefaults() {
+  static loadDefaults(coverallsFile = '') {
     let readYAML = file => new Promise((resolve, reject) => {
       fs.exists(file, (err, exists) => {
         if (err) reject(err);
@@ -139,7 +142,8 @@ export class Configuration {
       });
     });
 
-    return readYAML(`${process.cwd()}/.coveralls.yml`).then(config => {
+    let path = coverallsFile.length ? coverallsFile : `${process.cwd()}/.coveralls.yml`;
+    return readYAML(path).then(config => {
       let defaults = new Configuration();
       if (config) defaults.merge(config);
       defaults.merge(Configuration.fromEnvironment());
@@ -148,7 +152,7 @@ export class Configuration {
   }
 
   /**
-   * Adds all key-value pairs of the specified configuration to this one.
+   * Adds all entries of the specified configuration to this one.
    * @param {Configuration} config The configuration to be merged.
    */
   merge(config) {
@@ -158,10 +162,14 @@ export class Configuration {
   /**
    * Sets the value of the configuration parameter with the specified name.
    * @param {string} name The name of the configuration parameter.
+
+  /**
+   * Associates a given value to the specified key.
+   * @param {string} key The key to seek for.
    * @param {*} value The parameter value.
    */
-  set(name, value) {
-    this._params[name] = value;
+  set(key, value) {
+    this._params[key] = value;
   }
 
   /**
