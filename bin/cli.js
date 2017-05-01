33dd64e25a3cb7c5c7617ccf14f2ef3aca18d214
Cédric Belin <cedric@belin.io>
2017-05-01 17:59:16 +0200

    Code optimization

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 'use strict';
 
-const fs = require('fs');
-const pkg = require('../package.json');
-const path = require('path');
+const {access, readFile} = require('fs');
+const {version: pkgVersion} = require('../package.json');
+const {normalize} = require('path');
 const program = require('commander');
 const {Client} = require('../lib');
 
@@ -19,23 +19,23 @@ async function main() {
   // Parse the command line arguments.
   program
     .description('Send a LCOV coverage report to the Coveralls service.')
-    .version(pkg.version, '-v, --version')
+    .version(pkgVersion, '-v, --version')
     .option('-f, --file <file>', 'path to the coverage report');
 
   program.parse(process.argv);
   if (!program.file) program.help();
 
   // Check the report existence.
-  const fileExists = file => new Promise(resolve => fs.access(file, err => resolve(!err)));
+  const fileExists = file => new Promise(resolve => access(file, err => resolve(!err)));
 
-  let file = path.normalize(program.file);
+  let file = normalize(program.file);
   if (!await fileExists(file)) throw new Error(`The specified file is not found: ${file}`);
 
   // Upload the report to Coveralls.
-  const readFile = file => new Promise(resolve => fs.readFile(file, 'utf8', (err, data) => resolve(err ? '' : data)));
+  const loadReport = file => new Promise(resolve => readFile(file, 'utf8', (err, data) => resolve(err ? '' : data)));
 
   let client = new Client('COVERALLS_ENDPOINT' in process.env ? process.env.COVERALLS_ENDPOINT : Client.DEFAULT_ENDPOINT);
-  let coverage = await readFile(file);
+  let coverage = await loadReport(file);
 
   console.log('[Coveralls] Submitting to', client.endPoint);
   return client.upload(coverage);
