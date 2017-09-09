a21bb5a5943d497f5b7147a4e659ad0c76163c61
Cédric Belin <cedric@belin.io>
2017-09-09 14:43:40 +0200

    Code formatting

--- bin/cli.js
+++ bin/cli.js
@@ -26,9 +26,9 @@ async function main() {
 
   // Run the program.
   const loadReport = promisify(readFile);
-  let coverage = await loadReport(program.file, 'utf8');
 
   let client = new Client('COVERALLS_ENDPOINT' in process.env ? process.env.COVERALLS_ENDPOINT : Client.DEFAULT_ENDPOINT);
+  let coverage = await loadReport(program.file, 'utf8');
   console.log(`[Coveralls] Submitting to ${client.endPoint}`);
   return client.upload(coverage);
 }
