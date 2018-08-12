7b80d84a0b1157c1a878a0a918defc9621148bee
Cédric Belin <cedric@belin.io>
2018-08-12 17:31:14 +0200

    Ported the classes to TypeScript

--- src/client.ts
+++ src/client.ts
@@ -1,36 +1,20 @@
-const {which} from '@cedx/which');
-const EventEmitter from 'events');
-const FormData from 'form-data');
-const fetch from 'node-fetch');
-
-const {Configuration} from './configuration.js');
-const {GitCommit, GitData} from './git.js');
+import {which} from '@cedx/which';
+import {EventEmitter} from 'events';
+import * as FormData from 'form-data';
+import fetch, {Request} from 'node-fetch';
+import {Configuration} from './configuration';
+import {GitCommit, GitData} from './git';
+import {Job} from './job';
 
 /**
- * An exception caused by an error in a `Client` request.
+ * Uploads code coverage reports to the [Coveralls](https://coveralls.io) service.
  */
-class ClientError extends Error {
+export class Client extends EventEmitter {
 
   /**
-   * Creates a new client error.
-   * @param {string} message A message describing the error.
-   * @param {string|URL} [uri] The URL of the HTTP request or response that failed.
+   * The URL of the default API end point.
    */
-  constructor(message, uri) {
-    super(message);
-
-    /**
-     * The error name.
-     * @type {string}
-     */
-    this.name = 'ClientError';
-
-    /**
-     * The URL of the HTTP request or response that failed.
-     * @type {URL}
-     */
-    this.uri = typeof uri == 'string' ? new URL(uri) : uri;
-  }
+  public static readonly defaultEndPoint: URL = new URL('https://coveralls.io');
 
   /**
    * An event that is triggered when a request is made to the remote service.
@@ -45,30 +29,21 @@ class ClientError extends Error {
   public static readonly eventResponse: string = 'response';
 
   /**
-   * The URL of the default API end point.
-   * @type {URL}
+   * The URL of the API end point.
    */
-  static get defaultEndPoint() {
-    return new URL('https://coveralls.io');
-  }
+  public endPoint: URL;
 
   /**
    * Initializes a new instance of the class.
-   * @param {string|URL} [endPoint] The URL of the API end point.
+   * @param endPoint The URL of the API end point.
    */
-  constructor(endPoint = Client.defaultEndPoint) {
+  constructor(endPoint: string | URL = Client.defaultEndPoint) {
     super();
-
-    /**
-     * The URL of the API end point.
-     * @type {URL}
-     */
     this.endPoint = typeof endPoint == 'string' ? new URL(endPoint) : endPoint;
   }
 
   /**
    * The class name.
-   * @type {string}
    */
   get [Symbol.toStringTag](): string {
     return 'Client';
@@ -76,25 +51,23 @@ class ClientError extends Error {
 
   /**
    * Uploads the specified code coverage report to the Coveralls service.
-   * @param {string} coverage A coverage report.
-   * @param {Configuration} [configuration] The environment settings.
-   * @return {Promise} Completes when the operation is done.
-   * @emits {fetch~Request} The "request" event.
-   * @emits {fetch~Response} The "response" event.
+   * @param coverage A coverage report.
+   * @param configuration The environment settings.
+   * @return Completes when the operation is done.
    */
-  async upload(coverage, configuration = null) {
+  public async upload(coverage: string, configuration?: Configuration): Promise<void> {
     const report = coverage.trim();
     if (!report.length) throw new TypeError('The specified coverage report is empty.');
 
-    const job;
+    let job: Job | undefined;
     if (report.substring(0, 5) == '<?xml' || report.substring(0, 10) == '<coverage') {
-      const {parseReport} from './parsers/clover.js');
+      const {parseReport} = require('./parsers/clover');
       job = await parseReport(report);
     }
     else {
       const token = report.substring(0, 3);
       if (token == 'TN:' || token == 'SF:') {
-        const {parseReport} from './parsers/lcov.js');
+        const {parseReport} = require('./parsers/lcov');
         job = await parseReport(report);
       }
     }
@@ -118,47 +91,44 @@ class ClientError extends Error {
 
   /**
    * Uploads the specified job to the Coveralls service.
-   * @param {Job} job The job to be uploaded.
-   * @return {Promise} Completes when the operation is done.
-   * @emits {fetch~Request} The "request" event.
-   * @emits {fetch~Response} The "response" event.
+   * @param job The job to be uploaded.
+   * @return Completes when the operation is done.
    */
-  async uploadJob(job) {
+  public async uploadJob(job: Job): Promise<void> {
     if (!job.repoToken.length && !job.serviceName.length) throw new TypeError('The job does not meet the requirements.');
 
     const body = new FormData;
     body.append('json_file', Buffer.from(JSON.stringify(job)), 'coveralls.json');
 
-    const req = new fetch.Request(new URL('api/v1/jobs', this.endPoint).href, {method: 'POST', body});
-    this.emit('request', req);
+    const req = new Request(new URL('api/v1/jobs', this.endPoint).href, {method: 'POST', body});
+    this.emit(Client.eventRequest, req);
 
     const res = await fetch(req);
-    this.emit('response', res);
+    this.emit(Client.eventResponse, req, res);
 
     if (!res.ok) throw new ClientError('An error occurred while uploading the report.');
-    return res.text();
   }
 
   /**
    * Updates the properties of the specified job using the given configuration parameters.
-   * @param {Job} job The job to update.
-   * @param {Configuration} config The parameters to define.
+   * @param job The job to update.
+   * @param config The parameters to define.
    */
-  _updateJob(job, config) {
-    if (config.has('repo_token')) job.repoToken = config.get('repo_token');
-    else if (config.has('repo_secret_token')) job.repoToken = config.get('repo_secret_token');
+  private _updateJob(job: Job, config: Configuration): void {
+    if (config.has('repo_token')) job.repoToken = config.get('repo_token')!;
+    else if (config.has('repo_secret_token')) job.repoToken = config.get('repo_secret_token')!;
 
     if (config.has('parallel')) job.isParallel = config.get('parallel') == 'true';
-    if (config.has('run_at')) job.runAt = new Date(config.get('run_at'));
-    if (config.has('service_job_id')) job.serviceJobId = config.get('service_job_id');
-    if (config.has('service_name')) job.serviceName = config.get('service_name');
-    if (config.has('service_number')) job.serviceNumber = config.get('service_number');
-    if (config.has('service_pull_request')) job.servicePullRequest = config.get('service_pull_request');
+    if (config.has('run_at')) job.runAt = new Date(config.get('run_at')!);
+    if (config.has('service_job_id')) job.serviceJobId = config.get('service_job_id')!;
+    if (config.has('service_name')) job.serviceName = config.get('service_name')!;
+    if (config.has('service_number')) job.serviceNumber = config.get('service_number')!;
+    if (config.has('service_pull_request')) job.servicePullRequest = config.get('service_pull_request')!;
 
     const hasGitData = config.keys.some(key => key == 'service_branch' || key.substr(0, 4) == 'git_');
-    if (!hasGitData) job.commitSha = config.has('commit_sha') ? config.get('commit_sha') : '';
+    if (!hasGitData) job.commitSha = config.has('commit_sha') ? config.get('commit_sha')! : '';
     else {
-      const commit = new GitCommit(config.has('commit_sha') ? config.get('commit_sha') : '', {
+      const commit = new GitCommit(config.has('commit_sha') ? config.get('commit_sha')! : '', {
         authorEmail: config.has('git_author_email') ? config.get('git_author_email') : '',
         authorName: config.has('git_author_name') ? config.get('git_author_name') : '',
         committerEmail: config.has('git_committer_email') ? config.get('git_committer_email') : '',
@@ -168,5 +138,37 @@ class ClientError extends Error {
 
       job.git = new GitData(commit, {branch: config.has('service_branch') ? config.get('service_branch') : ''});
     }
+  }
+}
+
+/**
+ * An exception caused by an error in a `Client` request.
+ */
+export class ClientError extends Error {
+
+  /**
+   * The URL of the HTTP request or response that failed.
+   */
+  public uri: URL | null;
+
+  /**
+   * Creates a new client error.
+   * @param message A message describing the error.
+   * @param uri The URL of the HTTP request or response that failed.
+   */
+  constructor(message: string = '', uri: null | string | URL = null) {
+    super(message);
+    this.name = 'ClientError';
+    this.uri = typeof uri == 'string' ? new URL(uri) : uri;
+  }
+
+  /**
+   * Returns a string representation of this object.
+   * @return The string representation of this object.
+   */
+  public toString(): string {
+    let values = `"${this.message}"`;
+    if (this.uri) values = `${values}, uri: "${this.uri.href}"`;
+    return `${this.name}(${values})`;
   }
 }
--- src/configuration.ts
+++ src/configuration.ts
@@ -1,27 +1,27 @@
-const {promises} from 'fs');
-const {safeLoad, YAMLException} from 'js-yaml');
+import {promises} from 'fs';
+import {safeLoad, YAMLException} from 'js-yaml';
+import {JsonMap, StringMap} from './map';
 
 /**
  * Provides access to the coverage settings.
  */
-class Configuration {
+export class Configuration {
+
+  /**
+   * The configuration parameters.
+   */
+  private _params: Map<string, string>;
 
   /**
    * Initializes a new instance of the class.
-   * @param {Object} [params] The configuration parameters.
+   * @param params The configuration parameters.
    */
-  constructor(params = {}) {
-
-    /**
-     * The configuration parameters.
-     * @type {Map<string, *>}
-     */
-    this._params = new Map(Object.entries(params));
+  constructor(params: StringMap = {}) {
+    this._params = new Map(Object.entries(params)) as Map<string, string>;
   }
 
   /**
    * The class name.
-   * @type {string}
    */
   get [Symbol.toStringTag](): string {
     return 'Configuration';
@@ -29,50 +29,50 @@ class Configuration {
 
   /**
    * Creates a new configuration from the variables of the specified environment.
-   * @param {Object} [env] A map providing environment variables. Defaults to `process.env`.
-   * @return {Configuration} The newly created configuration.
+   * @param env A map providing environment variables. Defaults to `process.env`.
+   * @return The newly created configuration.
    */
-  static fromEnvironment(env = process.env) {
+  public static fromEnvironment(env: StringMap = process.env): Configuration {
     const config = new this;
 
     // Standard.
-    const serviceName = typeof env.CI_NAME == 'string' ? env.CI_NAME : '';
+    const serviceName = 'CI_NAME' in env ? env.CI_NAME! : '';
     if (serviceName.length) config.set('service_name', serviceName);
 
-    if ('CI_BRANCH' in env) config.set('service_branch', env.CI_BRANCH);
-    if ('CI_BUILD_NUMBER' in env) config.set('service_number', env.CI_BUILD_NUMBER);
-    if ('CI_BUILD_URL' in env) config.set('service_build_url', env.CI_BUILD_URL);
-    if ('CI_COMMIT' in env) config.set('commit_sha', env.CI_COMMIT);
-    if ('CI_JOB_ID' in env) config.set('service_job_id', env.CI_JOB_ID);
+    if ('CI_BRANCH' in env) config.set('service_branch', env.CI_BRANCH!);
+    if ('CI_BUILD_NUMBER' in env) config.set('service_number', env.CI_BUILD_NUMBER!);
+    if ('CI_BUILD_URL' in env) config.set('service_build_url', env.CI_BUILD_URL!);
+    if ('CI_COMMIT' in env) config.set('commit_sha', env.CI_COMMIT!);
+    if ('CI_JOB_ID' in env) config.set('service_job_id', env.CI_JOB_ID!);
 
     if ('CI_PULL_REQUEST' in env) {
-      const matches = /(\d+)$/.exec(env.CI_PULL_REQUEST);
+      const matches = /(\d+)$/.exec(env.CI_PULL_REQUEST!);
       if (matches && matches.length >= 2) config.set('service_pull_request', matches[1]);
     }
 
     // Coveralls.
     if ('COVERALLS_REPO_TOKEN' in env || 'COVERALLS_TOKEN' in env)
-      config.set('repo_token', env.COVERALLS_REPO_TOKEN ? env.COVERALLS_REPO_TOKEN : env.COVERALLS_TOKEN);
+      config.set('repo_token', env.COVERALLS_REPO_TOKEN ? env.COVERALLS_REPO_TOKEN! : env.COVERALLS_TOKEN!);
 
-    if ('COVERALLS_COMMIT_SHA' in env) config.set('commit_sha', env.COVERALLS_COMMIT_SHA);
-    if ('COVERALLS_PARALLEL' in env) config.set('parallel', env.COVERALLS_PARALLEL);
-    if ('COVERALLS_RUN_AT' in env) config.set('run_at', env.COVERALLS_RUN_AT);
-    if ('COVERALLS_SERVICE_BRANCH' in env) config.set('service_branch', env.COVERALLS_SERVICE_BRANCH);
-    if ('COVERALLS_SERVICE_JOB_ID' in env) config.set('service_job_id', env.COVERALLS_SERVICE_JOB_ID);
-    if ('COVERALLS_SERVICE_NAME' in env) config.set('service_name', env.COVERALLS_SERVICE_NAME);
+    if ('COVERALLS_COMMIT_SHA' in env) config.set('commit_sha', env.COVERALLS_COMMIT_SHA!);
+    if ('COVERALLS_PARALLEL' in env) config.set('parallel', env.COVERALLS_PARALLEL!);
+    if ('COVERALLS_RUN_AT' in env) config.set('run_at', env.COVERALLS_RUN_AT!);
+    if ('COVERALLS_SERVICE_BRANCH' in env) config.set('service_branch', env.COVERALLS_SERVICE_BRANCH!);
+    if ('COVERALLS_SERVICE_JOB_ID' in env) config.set('service_job_id', env.COVERALLS_SERVICE_JOB_ID!);
+    if ('COVERALLS_SERVICE_NAME' in env) config.set('service_name', env.COVERALLS_SERVICE_NAME!);
 
     // Git.
-    if ('GIT_AUTHOR_EMAIL' in env) config.set('git_author_email', env.GIT_AUTHOR_EMAIL);
-    if ('GIT_AUTHOR_NAME' in env) config.set('git_author_name', env.GIT_AUTHOR_NAME);
-    if ('GIT_BRANCH' in env) config.set('service_branch', env.GIT_BRANCH);
-    if ('GIT_COMMITTER_EMAIL' in env) config.set('git_committer_email', env.GIT_COMMITTER_EMAIL);
-    if ('GIT_COMMITTER_NAME' in env) config.set('git_committer_name', env.GIT_COMMITTER_NAME);
-    if ('GIT_ID' in env) config.set('commit_sha', env.GIT_ID);
-    if ('GIT_MESSAGE' in env) config.set('git_message', env.GIT_MESSAGE);
+    if ('GIT_AUTHOR_EMAIL' in env) config.set('git_author_email', env.GIT_AUTHOR_EMAIL!);
+    if ('GIT_AUTHOR_NAME' in env) config.set('git_author_name', env.GIT_AUTHOR_NAME!);
+    if ('GIT_BRANCH' in env) config.set('service_branch', env.GIT_BRANCH!);
+    if ('GIT_COMMITTER_EMAIL' in env) config.set('git_committer_email', env.GIT_COMMITTER_EMAIL!);
+    if ('GIT_COMMITTER_NAME' in env) config.set('git_committer_name', env.GIT_COMMITTER_NAME!);
+    if ('GIT_ID' in env) config.set('commit_sha', env.GIT_ID!);
+    if ('GIT_MESSAGE' in env) config.set('git_message', env.GIT_MESSAGE!);
 
     // CI services.
-    const merge = service => {
-      const {getConfiguration} from `./services/${service}`);
+    const merge = (service: string) => {
+      const {getConfiguration} = require(`./services/${service}`);
       config.merge(getConfiguration(env));
     };
 
@@ -95,16 +95,16 @@ class Configuration {
 
   /**
    * Creates a new source file from the specified YAML document.
-   * @param {string} document A YAML document providing configuration parameters.
-   * @return {Configuration} The instance corresponding to the specified YAML document.
+   * @param document A YAML document providing configuration parameters.
+   * @return The instance corresponding to the specified YAML document.
    * @throws {TypeError} The specified document is invalid.
    */
-  static fromYaml(document) {
-    if (typeof document != 'string' || !document.trim().length) throw new TypeError('The specified YAML document is empty.');
+  public static fromYaml(document: string): Configuration {
+    if (!document.trim().length) throw new TypeError('The specified YAML document is empty.');
 
     try {
       const map = safeLoad(document);
-      if (!map || typeof map != 'object') throw new TypeError('The specified YAML document is invalid.');
+      if (typeof map != 'object' || !map) throw new TypeError('The specified YAML document is invalid.');
       return new this(map);
     }
 
@@ -117,10 +117,10 @@ class Configuration {
   /**
    * Loads the default configuration.
    * The default values are read from the environment variables and an optional `.coveralls.yml` file.
-   * @param {string} [coverallsFile] The path to the `.coveralls.yml` file. Defaults to the file found in the current directory.
-   * @return {Promise<Configuration>} The default configuration.
+   * @param coverallsFile The path to the `.coveralls.yml` file. Defaults to the file found in the current directory.
+   * @return The default configuration.
    */
-  static async loadDefaults(coverallsFile = '.coveralls.yml') {
+  public static async loadDefaults(coverallsFile: string = '.coveralls.yml'): Promise<Configuration> {
     const defaults = Configuration.fromEnvironment();
 
     try {
@@ -135,71 +135,65 @@ class Configuration {
 
   /**
    * The keys of this configuration.
-   * @type {string[]}
    */
-  get keys() {
-    const keys = [];
-    keys.push(...this._params.keys());
-    return keys;
+  get keys(): string[] {
+    return Array.from(this._params.keys());
   }
 
   /**
    * The number of entries in this configuration.
-   * @type {number}
    */
-  get length() {
+  get length(): number {
     return this._params.size;
   }
 
   /**
    * Returns a new iterator that allows iterating the entries of this configuration.
-   * @return {Iterator<Array>} An iterator for the entries of this configuration.
    */
-  [Symbol.iterator]() {
+  public [Symbol.iterator](): Iterator<[string, string]> {
     return this._params.entries();
   }
 
   /**
    * Gets the value associated to the specified key.
-   * @param {string} key The key to seek for.
-   * @param {*} defaultValue The default parameter value if it does not exist.
-   * @return {*} The value of the configuration parameter, or the default value if the parameter is not found.
+   * @param key The key to seek for.
+   * @return The value of the configuration parameter, or the default value if the parameter is not found.
    */
-  get(key, defaultValue = null) {
-    return this.has(key) ? this._params.get(key) : defaultValue;
+  public get(key: string): string | undefined {
+    return this._params.get(key);
   }
 
   /**
    * Gets a value indicating whether this configuration contains the specified key.
-   * @param {string} key The key to seek for.
-   * @return {boolean} `true` if this configuration contains the specified key, otherwise `false`.
+   * @param key The key to seek for.
+   * @return `true` if this configuration contains the specified key, otherwise `false`.
    */
-  has(key) {
+  public has(key: string): boolean {
     return this._params.has(key);
   }
 
   /**
    * Adds all entries of the specified configuration to this one.
-   * @param {Configuration} config The configuration to be merged.
+   * @param config The configuration to be merged.
    */
-  merge(config) {
+  public merge(config: Configuration): void {
     for (const [key, value] of config) this.set(key, value);
   }
 
   /**
    * Removes the value associated to the specified key.
-   * @param {string} key The key to seek for.
+   * @param key The key to seek for.
    */
-  remove(key) {
+  public remove(key: string): void {
     this._params.delete(key);
   }
 
   /**
    * Associates a given value to the specified key.
-   * @param {string} key The key to seek for.
-   * @param {*} value The parameter value.
+   * @param key The key to seek for.
+   * @param value The parameter value.
    */
-  set(key, value) {
+  public set(key: string, value: string) {
     this._params.set(key, value);
   }
 
@@ -207,8 +201,8 @@ class Configuration {
    * Converts this object to a map in JSON format.
    * @return The map in JSON format corresponding to this object.
    */
-  public toJSON(): {[key: string]: any} {
-    const map = {};
+  public toJSON(): JsonMap {
+    const map: JsonMap = {};
     for (const [key, value] of this) map[key] = value;
     return map;
   }
