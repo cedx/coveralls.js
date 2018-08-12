6358c5bc1fb498d83b6d4aaf8329f3fc65416110
Cédric Belin <cedric@belin.io>
2018-08-12 17:42:18 +0200

    Ported the parser tests to TS

--- test/parsers/clover_test.ts
+++ test/parsers/clover_test.ts
@@ -1,3 +1,4 @@
+/* tslint:disable: no-unused-expression */
 import {expect} from 'chai';
 import {promises} from 'fs';
 import {suite, test} from 'mocha-typescript';
@@ -14,32 +15,32 @@ import {parseReport} from '../../src/parsers/clover';
   /**
    * @test {parseReport}
    */
-  describe('parseReport()', () => {
-    // It should properly parse Clover reports', async () => {
-      const job = await parseReport(await promises.readFile('test/fixtures/clover.xml', 'utf8'));
-      expect(job.sourceFiles).to.be.an('array').and.have.lengthOf(3);
+  @test('It should properly parse Clover reports')
+  public async parseReport(): Promise<void> {
+    const job = await parseReport(await promises.readFile('test/fixtures/clover.xml', 'utf8'));
+    expect(job.sourceFiles).to.be.an('array').and.have.lengthOf(3);
 
-      expect(job.sourceFiles[0]).to.be.instanceof(SourceFile);
-      expect(job.sourceFiles[0].name).to.equal(join('lib', 'client.js'));
-      expect(job.sourceFiles[0].sourceDigest).to.not.be.empty;
-      expect(job.sourceFiles[0].coverage).to.include.members([null, 2, 2, 2, 2, null]);
+    expect(job.sourceFiles[0]).to.be.instanceof(SourceFile);
+    expect(job.sourceFiles[0].name).to.equal(join('lib', 'client.js'));
+    expect(job.sourceFiles[0].sourceDigest).to.not.be.empty;
+    expect(job.sourceFiles[0].coverage).to.include.members([null, 2, 2, 2, 2, null]);
 
-      expect(job.sourceFiles[1].name).to.equal(join('lib', 'configuration.js'));
-      expect(job.sourceFiles[1].sourceDigest).to.not.be.empty;
-      expect(job.sourceFiles[1].coverage).to.include.members([null, 4, 4, 2, 2, 4, 2, 2, 4, 4, null]);
+    expect(job.sourceFiles[1].name).to.equal(join('lib', 'configuration.js'));
+    expect(job.sourceFiles[1].sourceDigest).to.not.be.empty;
+    expect(job.sourceFiles[1].coverage).to.include.members([null, 4, 4, 2, 2, 4, 2, 2, 4, 4, null]);
 
-      expect(job.sourceFiles[2].name).to.equal(join('lib', 'git.js'));
-      expect(job.sourceFiles[2].sourceDigest).to.not.be.empty;
-      expect(job.sourceFiles[2].coverage).to.include.members([null, 2, 2, 2, 2, 2, 0, 0, 2, 2, null]);
+    expect(job.sourceFiles[2].name).to.equal(join('lib', 'git.js'));
+    expect(job.sourceFiles[2].sourceDigest).to.not.be.empty;
+    expect(job.sourceFiles[2].coverage).to.include.members([null, 2, 2, 2, 2, 2, 0, 0, 2, 2, null]);
 
     // It should throw an excepton if the Clover report is invalid', async () => {
-      try {
-        await parseReport('<coverage><foo /></coverage>');
-        expect.fail('Error not thrown');
-      }
+    try {
+      await parseReport('<coverage><foo /></coverage>');
+      expect.fail('Error not thrown');
+    }
 
-      catch (err) {
-        expect(true).to.be.ok;
-      }
+    catch (err) {
+      expect(err).to.be.instanceof(TypeError);
+    }
   }
 }
