1c460d149382b23406f5304ff33feec5df7dfe00
Cédric Belin <cedric@belin.io>
2019-08-01 15:02:24 +0200

    Fixed some tests

--- test/parsers/clover_test.ts
+++ test/parsers/clover_test.ts
@@ -15,15 +15,15 @@ describe('Clover', () => {
       expect(job.sourceFiles).to.be.an('array').and.have.lengthOf(3);
 
       expect(job.sourceFiles[0]).to.be.an.instanceof(SourceFile);
-      expect(job.sourceFiles[0].name).to.equal(join('lib', 'client.js'));
+      expect(job.sourceFiles[0].name).to.equal(join('src', 'io', 'client.ts'));
       expect(job.sourceFiles[0].sourceDigest).to.not.be.empty;
       expect(job.sourceFiles[0].coverage).to.include.members([null, 2, 2, 2, 2, null]);
 
-      expect(job.sourceFiles[1].name).to.equal(join('lib', 'configuration.js'));
+      expect(job.sourceFiles[1].name).to.equal(join('src', 'io', 'configuration.ts'));
       expect(job.sourceFiles[1].sourceDigest).to.not.be.empty;
       expect(job.sourceFiles[1].coverage).to.include.members([null, 4, 4, 2, 2, 4, 2, 2, 4, 4, null]);
 
-      expect(job.sourceFiles[2].name).to.equal(join('lib', 'git.js'));
+      expect(job.sourceFiles[2].name).to.equal(join('src', 'io', 'git.ts'));
       expect(job.sourceFiles[2].sourceDigest).to.not.be.empty;
       expect(job.sourceFiles[2].coverage).to.include.members([null, 2, 2, 2, 2, 2, 0, 0, 2, 2, null]);
     });
