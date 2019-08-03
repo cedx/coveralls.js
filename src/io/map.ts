a9f2c98ac4e11070ac5735baa323f218fbe67150
Cédric Belin <cedric@belin.io>
2019-08-03 13:24:20 +0200

    Updated the documentation

--- src/io/map.ts
+++ src/io/map.ts
@@ -1,7 +1,10 @@
 /** Defines the shape of a map in JSON format. */
 export type JsonMap = StringMap<any>;
 
-/** Defines the shape of a dictionary whose keys are strings. */
+/**
+ * Defines the shape of a dictionary whose keys are strings.
+ * @typeparam T The type of the map values.
+ */
 export interface StringMap<T = string|undefined> {
 
   /** Gets or sets the value for the given key. */
