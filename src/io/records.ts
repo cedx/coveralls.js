4c537db6a040ac44c01b17f3788f6576f485d32d
Cédric Belin <cedric@belin.io>
2019-08-23 10:25:56 +0200

    Updated the documentation

--- src/io/records.ts
+++ src/io/records.ts
@@ -1,4 +1,4 @@
-/** Defines the shape of a map in JSON format. */
+/** Defines the shape of an object in JSON format. */
 export type JsonObject = Record<string, any>;
 
 /** Defines the shape of a map whose values are strings. */
