56f11ee325e2f07d2ab0c4984b48756d8d46c572
Cédric Belin <cedric@belin.io>
2019-09-19 20:24:06 +0200

    Added the build URL to GitHub service

--- src/io/services/github.ts
+++ src/io/services/github.ts
@@ -8,13 +8,14 @@ import {StringMap} from '../records';
  * @return The configuration parameters.
  */
 export function getConfiguration(env: StringMap): Configuration {
-  const commitSha = 'GITHUB_SHA' in env ? env.GITHUB_SHA! : '';
+  const commitSha = env.GITHUB_SHA;
   const gitRef = 'GITHUB_REF' in env ? env.GITHUB_REF! : '';
+  const repository = env.GITHUB_REPOSITORY;
 
   return new Configuration({
-    commit_sha: commitSha.length ? commitSha : undefined,
+    commit_sha: commitSha ? commitSha : undefined,
     service_branch: gitRef.startsWith('refs/heads/') ? gitRef.substring(11) : undefined,
-    service_build_url: commitSha.length ? `https://github.com/cedx/coveralls.js/commit/${commitSha}/checks` : undefined,
+    service_build_url: commitSha && repository ? `https://github.com/${repository}/commit/${commitSha}/checks` : undefined,
     service_name: 'github'
   });
 }
